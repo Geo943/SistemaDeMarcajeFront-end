bec32a05e810115b907347a72622c3dcc983840f
Geo943 <ghernandezf1@miumg.edu.gt>
2022-09-05 00:55:52 -0600

    comodin de url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
   
 })
 export class AppComponent {
